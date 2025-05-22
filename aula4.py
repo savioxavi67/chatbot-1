3d95da57bb6e64d88deaf1d1b715d948b21f75e1
Star8090 <eu.sxfurtado@gmail.com>
2025-05-22 13:40:04 -0400

    Create aula4.py

---
+++ aula4.py
@@ -0,0 +1,72 @@
+numero1 = 10
+numero2 = 50
+texto1 = "Ctrl"
+texto2 = "Play"
+
+print(numero1 == numero2)
+print(numero1 != numero2)
+print(numero1 == texto1)
+print(8 < 9 < 10 < 11)
+print(9 > 10 and 10 > 11)
+print(9 > 10 or 10 > 11)
+
+if numero1 < numero2:
+
+    print("O numero1 é menor que o numero2")
+elif numero1 == numero2:
+    print("Os numeros são iguais")
+else:
+    print("O numero2 é maior que o numero1")
+
+nota = 7
+
+if nota >= 6
+    print("Foi Aprovado")
+elif  nota >= 5 and nota <= 6:
+    print("Recuperação")
+else:
+    print("Foi Reprovado")
+
+mes = int(input())
+
+if mes == 1
+    print("Janeiro")
+elif mes == 2
+    print("Fevereiro")
+elif mes == 3
+    print("Março")
+elif mes == 4
+    print("Abril")
+elif mes == 5
+    print("Maio")
+elif mes == 6
+    print("Junho")
+elif mes == 7
+    print("Julho")
+elif mes == 8
+    print("Agosto")
+elif mes == 9
+    print("Setembro")
+elif mes == 10
+    print("Outubro")
+elif mes == 11
+    print("Novembro")
+elif mes == 12
+    print("Dezembro")
+else:
+    print("Número de mês inválido")
+
+cod = int(input())
+qnt = int(input())
+
+if cod == 1:
+    print("Total: R$", qnt * 10)
+elif cod == 2:
+    print("Total: R$", qnt * 15)
+elif cod == 3:
+    print("Total: R$", qnt * 18)
+elif cod == 4:
+    print("Total: R$", qnt * 5)
+elif cod == 5:
+    print("Total: R$", qnt * 4)
+
